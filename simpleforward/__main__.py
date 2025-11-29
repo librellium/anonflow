2e1dc1c54c4fc3006424f17fd952c4d2a89656af
Librellium <246878136+librellium@users.noreply.github.com>
2025-11-29 20:55:07 +0500

    Add moderator instance creation

--- simpleforward/__main__.py
+++ simpleforward/__main__.py
@@ -5,14 +5,16 @@ from aiogram import Bot, Dispatcher
 
 from simpleforward.bot import MessageManager, build
 from simpleforward.config import Config
+from simpleforward.moderation import AsyncModerator, RuleManager
 
 from . import paths
 
 
 async def main():
-    config = Config.load(paths.CONFIG_FILE)
+    if not paths.CONFIG_FILE.exists():
+        Config().save(paths.CONFIG_FILE)
 
-    message_manager = MessageManager()
+    config = Config.load(paths.CONFIG_FILE)
 
     logging.basicConfig(format=config.logging.fmt,
                         datefmt=config.logging.date_fmt,
@@ -21,8 +23,16 @@ async def main():
     bot = Bot(token=config.bot.token.get_secret_value())
     dispatcher = Dispatcher()
 
-    dispatcher.include_router(build(config.forwarding,
-                                    message_manager))
+    message_manager = MessageManager()
+
+    moderator = None
+    if config.moderation.enabled:
+        rule_manager = RuleManager(paths.RULES_DIR)
+        moderator = AsyncModerator(config, rule_manager)
+
+    dispatcher.include_router(
+        build(config, message_manager, moderator)
+    )
 
     await dispatcher.start_polling(bot)
 
