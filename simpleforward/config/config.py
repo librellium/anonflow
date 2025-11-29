831f22eec69e82bab3ab8a764658a021a3aa1952
Librellium <246878136+librellium@users.noreply.github.com>
2025-11-29 20:08:54 +0500

    Refactor Config for readability

--- simpleforward/config/config.py
+++ simpleforward/config/config.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Optional
 
 import yaml
 from pydantic import BaseModel, SecretStr
@@ -13,36 +12,6 @@ class Config(BaseModel):
     logging: Logging = Logging()
 
     @classmethod
-    def load(cls, cfg_path: Optional[Path] = None):
-        filepath = Path(cfg_path)
-        filepath.parent.mkdir(parents = True, exist_ok = True)
-
-        if not filepath.exists():
-            config = cls()
-        else:
-            with filepath.open(encoding = "utf-8") as config_file:
-                config = cls(**yaml.safe_load(config_file))
-
-        object.__setattr__(config, "filepath", filepath)
-
-        if not filepath.exists():
-            config.save()
-
-        return config
-
-    def save(self, cfg_path: Optional[Path] = None):
-        if not getattr(self, "filepath") and not cfg_path:
-            raise FileNotFoundError(f"Cannot find config file: {getattr(self.filepath) or cfg_path}")
-
-        if not getattr(self, "filepath") or (not isinstance(cfg_path, Path) and cfg_path is not None):
-            self.filepath = Path(cfg_path)
-
-        self.filepath.parent.mkdir(parents = True, exist_ok = True)
-
-        with open(self.filepath, "w", encoding = "utf-8") as config_file:
-            yaml.dump(self._serialize(self.model_dump()), config_file, width = float("inf"), sort_keys = False)
-
-    @classmethod
     def _serialize(cls, obj):
         if isinstance(obj, SecretStr):
             return obj.get_secret_value()
@@ -52,3 +21,22 @@ class Config(BaseModel):
             return [cls._serialize(value) for value in obj]
 
         return obj
+
+    @classmethod
+    def load(cls, filepath: Path):
+        filepath = Path(filepath)
+        filepath.parent.mkdir(parents = True, exist_ok = True)
+
+        config = cls()
+
+        if filepath.exists():
+            with filepath.open(encoding = "utf-8") as f:
+                config = cls(**yaml.safe_load(f))
+
+        return config
+
+    def save(self, filepath: Path):
+        filepath.parent.mkdir(parents = True, exist_ok = True)
+
+        with filepath.open("w", encoding="utf-8") as config_file:
+            yaml.dump(self._serialize(self.model_dump()), config_file, width = float("inf"), sort_keys = False)
