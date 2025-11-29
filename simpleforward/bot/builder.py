adb818f995fbec2142afbd6d779386db4e9850ae
Librellium <246878136+librellium@users.noreply.github.com>
2025-11-29 20:41:09 +0500

    Rename main.py

--- simpleforward/bot/builder.py
+++ simpleforward/bot/builder.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 
-from simpleforward.config import models
+from simpleforward.config import Config
 
 from .message_manager import MessageManager
 from .routers.photo import PhotoRouter
@@ -8,16 +8,17 @@ from .routers.start import StartRouter
 from .routers.text import TextRouter
 
 
-def build(forwarding_config: models.Forwarding,
-          message_manager: MessageManager):
+def build(config: Config, message_manager: MessageManager):
     main_router = Router()
 
     main_router.include_routers(
         StartRouter(),
-        TextRouter(forwarding_config,
-                   message_manager),
-        PhotoRouter(forwarding_config,
-                    message_manager)
+        TextRouter(
+            config, message_manager
+        ),
+        PhotoRouter(
+            config, message_manager
+        )
     )
 
     return main_router
