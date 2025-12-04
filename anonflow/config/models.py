0d27b2ec39741dbb2bc3f934961d7ad92e0ce387
Librellium <246878136+librellium@users.noreply.github.com>
2025-12-04 21:04:27 +0500

    Update config models
    - Add omni moderation type
    - Remove message template

--- anonflow/config/models.py
+++ anonflow/config/models.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, SecretStr
 
 ForwardingMode: TypeAlias = Literal["both", "moderation", "publication"]
 ForwardingType: TypeAlias = Literal["text", "photo", "video"]
-ModerationType: TypeAlias = Literal["gpt"]
+ModerationType: TypeAlias = Literal["omni", "gpt"]
 LoggingLevel: TypeAlias = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
 class Bot(BaseModel):
@@ -15,7 +15,6 @@ class Forwarding(BaseModel):
     mode: Optional[ForwardingMode] = "both"
     moderation_chat_id: Optional[int] = None
     publication_chat_id: Optional[int] = None
-    message_template: str = "🔔 У вас новое сообщение!\n\n<blockquote>{text}</blockquote>"
     types: List[ForwardingType] = ["text", "photo", "video"]
 
 class OpenAI(BaseModel):
@@ -26,7 +25,7 @@ class OpenAI(BaseModel):
 class Moderation(BaseModel):
     enabled: bool = True
     model: str = "gpt-5-mini"
-    types: List[ModerationType] = ["gpt"]
+    types: List[ModerationType] = ["omni", "gpt"]
 
 class Logging(BaseModel):
     level: LoggingLevel = "INFO"
