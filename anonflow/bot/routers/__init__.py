315c32d1cb60a084fc59515c005460126eb274e3
Librellium <246878136+librellium@users.noreply.github.com>
2025-12-12 20:34:05 +0500

    Update __init__

--- anonflow/bot/routers/__init__.py
+++ anonflow/bot/routers/__init__.py
@@ -1,5 +1,6 @@
+from .info import InfoRouter
 from .media import MediaRouter
 from .start import StartRouter
 from .text import TextRouter
 
-__all__ = ["MediaRouter", "StartRouter", "TextRouter"]
+__all__ = ["InfoRouter", "MediaRouter", "StartRouter", "TextRouter"]
