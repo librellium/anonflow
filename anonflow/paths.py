4c7daaba55b8d37af09dc38f88a6d247f7b3a029
Librellium <246878136+librellium@users.noreply.github.com>
2025-12-23 22:45:39 +0500

    Update paths.py

--- anonflow/paths.py
+++ anonflow/paths.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
-CONFIG_FILE = ROOT_DIR / "config.yml"
-CONFIG_EXAMPLE_FILE = ROOT_DIR / "config.yml.example"
+CONFIG_FILEPATH = ROOT_DIR / "config.yml"
+CONFIG_EXAMPLE_FILEPATH = ROOT_DIR / "config.yml.example"
 
 RULES_DIR = ROOT_DIR / "rules"
 
-TEMPLATES_DIR = ROOT_DIR / "templates"
+TRANSLATIONS_DIR = ROOT_DIR / "translations"
